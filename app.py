0e8caa511a25375fac3a73ecbe0cce50bd8c8005
Dustin Cremascoli <dustincremascoli@aol.com>
2023-11-09 10:44:28 -0600

    First commit to send all files to GitHub.

---
+++ app.py
@@ -0,0 +1,101 @@
+from os.path import dirname, join
+from pathlib import Path
+
+import psycopg2
+import pandas as pd
+import numpy as np
+
+from bokeh.io import curdoc
+from bokeh.layouts import column, row
+from bokeh.models import ColumnDataSource, Div, Select, Slider, TextInput, Range1d
+from bokeh.plotting import figure
+
+from models import stations, data, headers
+
+weatherdata = pd.DataFrame(data, columns=headers)
+weatherdata['date'] = np.array(weatherdata['date'], dtype=np.datetime64)
+
+axis_map = {
+    "Temperature": "tmp",
+    "Barometric Pressure": "slp",
+    "Wind Speed": "wnd",
+    "Dew Point": "dew",
+}
+
+y_axis_range = {
+    "Temperature": (-25.0, 125.0), 
+    "Barometric Pressure": (29.0, 31.0),
+    "Wind Speed": (0.0, 50.0),
+    "Precipitation": (0.0, 1.0),
+    "Dew Point": (0.0, 100.0),
+}
+
+desc = Div(text=(Path(__file__).parent / "description.html").read_text("utf8"), sizing_mode="stretch_width",
+           margin=(2, 2, 2, 15))
+
+years = Slider(title="Year", value=2023, start=2023, end=2023, step=1, margin=(2, 2, 2, 15))
+months = Slider(title="Month", value=1, start=1, end=12, step=1, margin=(2, 2, 2, 15))
+station_name = Select(title="Weather Station Name", value=stations[0], options=stations, margin=(2, 2, 2, 15))
+y_axis = Select(title="Main Chart Y Axis", options=sorted(axis_map.keys()), value="Barometric Pressure", margin=(2, 2, 2, 15))
+
+source = ColumnDataSource(data=dict(x=weatherdata[weatherdata['station_name'] == stations[0]]['date'],
+                                    y=weatherdata[weatherdata['station_name'] == stations[0]][axis_map[y_axis.value]]))
+
+plot = figure(height=400, title="", toolbar_location=None, sizing_mode="stretch_width",
+              x_axis_type="datetime", y_range=y_axis_range[y_axis.value], margin=(10, 10, 10, 10),
+              background_fill_color="#F9F7FA", outline_line_color='white')
+
+plot.line(x="x", y="y", source=source)
+
+source_precip = ColumnDataSource(data=dict(x=weatherdata[weatherdata['station_name'] == stations[0]]['date'],
+                                    y=weatherdata[weatherdata['station_name'] == stations[0]]['prp']))
+
+plot_precip = figure(height=150, width=400, title="", toolbar_location=None, sizing_mode="stretch_width",
+              x_axis_type="datetime", y_range=y_axis_range['Precipitation'], margin=(10, 10, 10, 10),
+              background_fill_color="#F1EDF5", outline_line_color='white')
+
+plot_precip.line(x="x", y="y", source=source_precip)
+plot_precip.yaxis.axis_label = "Precip"
+
+
+def select_weather_records():
+    selected = weatherdata[
+        (weatherdata.rdg_year == years.value) &
+        (weatherdata.rdg_month == months.value) &
+        (weatherdata.station_name == station_name.value)
+    ]
+    return selected
+
+
+def update():
+    df = select_weather_records()
+    y_name = axis_map[y_axis.value]
+    plot.yaxis.axis_label = y_axis.value
+    y_range_min = y_axis_range[y_axis.value][0]
+    y_range_max = y_axis_range[y_axis.value][1]
+    plot.y_range.update(start=y_range_min, end=y_range_max)
+    source.data = dict(
+        x=df['date'],
+        y=df[y_name],
+    )
+    source_precip.data = dict(
+        x=df['date'],
+        y=df['prp'],
+    )
+
+controls = [years, months, station_name, y_axis]
+for control in controls:
+    control.on_change('value', lambda attr, old, new: update())
+
+layout = column(desc,
+                row(years, months, sizing_mode="inherit"),
+                station_name,
+                y_axis,
+                plot,
+                plot_precip,
+                sizing_mode="stretch_width", height=400)
+
+update()
+
+curdoc().add_root(layout)
+curdoc().title = "Weather Data"
---
+++ models.py
@@ -0,0 +1,59 @@
+import psycopg2
+          
+dbname = 'weatherdata'
+user = 'dustincremascoli'
+password = '2J6oodf6lY1xEgssydxTR1Pb9QPNFBuj'
+host = 'dpg-cki06rcldqrs73f2cgng-a.ohio-postgres.render.com'
+port = 5432
+
+url_string = f"dbname={dbname} user={user} password={password} host={host} port={port}"
+
+query_stations = f'''
+SELECT DISTINCT loc.station_name
+FROM locations loc
+JOIN observations obs
+    ON loc.station = obs.station
+JOIN regions rgn
+    ON loc.state = rgn.state
+WHERE rgn.sub_region IN ('PACIFIC', 'EAST NORTH CENTRAL', 'NEW ENGLAND')
+ORDER BY loc.station_name;
+'''
+
+query_data = f'''
+SELECT
+    obs.station,
+    loc.station_name,
+    loc.state,
+    obs.date,
+    EXTRACT(YEAR from obs.date) AS rdg_year,
+    EXTRACT(MONTH from obs.date) AS rdg_month,
+    EXTRACT(DAY from obs.date) AS rdg_day,
+    obs.tmp,
+    obs.slp,
+    obs.wnd,
+    obs.prp,
+    obs.dew
+FROM observations obs
+JOIN locations loc
+    ON obs.station = loc.station
+JOIN regions rgn
+    ON loc.state = rgn.state
+WHERE rgn.sub_region IN ('PACIFIC', 'EAST NORTH CENTRAL', 'NEW ENGLAND')
+    AND EXTRACT(YEAR from obs.date) BETWEEN 2022 AND 2023
+    AND obs.source IN ('6', '7')
+    AND obs.report_type IN ('FM-15')
+    AND obs.slp BETWEEN 20.00 AND 35.00
+    AND obs.prp <= 10.00
+ORDER BY obs.station, obs.date;
+'''
+
+headers = ['station' , 'station_name', 'state', 'date', 'rdg_year', 'rdg_month', 'rdg_day', 'tmp', 'slp', 'wnd', 'prp', 'dew']
+
+with psycopg2.connect(url_string) as connection:
+    cursor = connection.cursor()
+    cursor.execute(query_stations)
+    response_stations = cursor.fetchall()
+    stations = [x[0] for x in response_stations]
+    cursor.execute(query_data)
+    response_data = cursor.fetchall()
+    data = [x for x in response_data]
